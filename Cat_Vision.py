2e5d0aaf31b5c6e800506f79a8ac83adb4ebcbdc
johnny-carp0s <joaodccarvalho@outlook.com>
2022-07-04 11:23:21 -0300

    first tutorial

--- Cat_Vision.py
+++ Cat_Vision.py
@@ -1,3 +1,12 @@
 import cv2
 
 print(cv2.__version__)
+
+img = cv2.imread("discord pfp.jpg", 1)
+img = cv2.resize(img, (0,0), fx=2,fy=2)
+img = cv2.rotate(img, cv2.ROTATE_180)
+cv2.imwrite('newimg.jpg', img)
+
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
